2500480e43f38961bcf7648591398819270dff14
kekul <S-HENTAY-S@yandex.ru>
2024-01-26 19:27:28 +0300

    Add frontend & gateway CI

--- backend/api/test.py
+++ backend/api/test.py
@@ -14,7 +14,7 @@ class TaskiAPITestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
-        """Проверка создания задач."""
+        """Проверка создания задачи."""
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
